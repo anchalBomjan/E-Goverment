4222c9478fd9ae74776921f4379d261f7fdfd648
anchal.lama <anchal.lama@imark.com.np>
2026-01-12 13:03:21 +0545

    Adding css files

--- gov/models.py
+++ gov/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
+    id = models.BigAutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=30)
